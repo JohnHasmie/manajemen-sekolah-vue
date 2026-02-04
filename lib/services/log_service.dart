1285b2e9d46c892a56ec006499b71839d2b2e1ce
luaygitaris <luaygitaris.com>
2026-02-04 14:37:30 +0700

    menambah secret key untuk log

--- lib/services/log_service.dart
+++ lib/services/log_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:manajemensekolah/services/api_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -50,10 +51,12 @@ class LogService {
         },
       };
 
+      final apiKey = dotenv.env['LOG_API_KEY'] ?? '';
+
       await http
           .post(
             Uri.parse(_logApiUrl),
-            headers: {'Content-Type': 'application/json'},
+            headers: {'Content-Type': 'application/json', 'x-api-key': apiKey},
             body: json.encode(body),
           )
           .timeout(const Duration(seconds: 5));
