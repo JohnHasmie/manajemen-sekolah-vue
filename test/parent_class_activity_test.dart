6ff74729a68e52ecc444dd17f9ac939a0d1dd81b
Yahya Hasymi <yahyahasymi@gmail.com>
2026-04-30 14:41:49 +0700

    chore(test): commit missing parent class activity test

---
+++ test/parent_class_activity_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:manajemensekolah/features/class_activity/presentation/screens/parent_class_activity_screen.dart';
+
+void main() {
+  testWidgets('Renders ParentClassActivityScreen without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: ParentClassActivityScreen(),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+    expect(find.byType(ParentClassActivityScreen), findsOneWidget);
+  });
+}
